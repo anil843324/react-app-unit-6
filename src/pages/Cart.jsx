3563404c77b29d95d955b66eed1cf050dcccaaea
anil843324 <anilkrsingh14200@gmail.com>
2022-09-29 13:15:17 +0530

    cart page almost done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,85 +1,120 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { cartList } from "../redux/cartAction";
-import { BsPlusCircle} from "react-icons/bs"
-import {AiOutlineMinusCircle ,AiOutlineDelete} from "react-icons/ai"
-const Cart = () => {
 
-  
+const Cart = () => {
   const dispatch = useDispatch();
 
-  
   const cartData = useSelector((state) => state.cartData);
 
   console.log(" Data fetch from saga ", cartData);
 
   useEffect(() => {
-    dispatch( cartList());
+    dispatch(cartList());
   }, []);
-
 
   return (
     <div>
       <div>
-        <div className="overflow-x-auto relative">
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
-                <th scope="col" className="py-3 px-6">
-                  Image
+                <th scope="col" class="py-3 px-6">
+                  <span class="sr-only">Image</span>
                 </th>
-                <th scope="col" className="py-3 px-6">
-                  Brand Name
+                <th scope="col" class="py-3 px-6">
+                  Product
                 </th>
-                <th scope="col" className="py-3 px-6">
-                 Category
+                <th scope="col" class="py-3 px-6">
+                  Qty
                 </th>
-                <th scope="col" className="py-3 px-6">
+                <th scope="col" class="py-3 px-6">
                   Price
-                </th>    
-                <th scope="col" className="py-3 px-6">
-                Decrease quantity
                 </th>
-                <th scope="col" className="py-3 px-6">
-                 Quantity
-                </th>
-                <th scope="col" className="py-3 px-6">
-                Increase quantity
-                </th>
-                <th scope="col" className="py-3 px-6">
-                Remove from cart
+                <th scope="col" class="py-3 px-6">
+                  Action
                 </th>
               </tr>
             </thead>
             <tbody>
 
-             {
-
-              cartData.map((el)=>(
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th
-                  scope="row"
-                  className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                 <img src={el.image} alt="" className=" h-16 w-16 rounded-full" />
-                </th>
-                <td className="py-4 px-6 text-gray-900">{el.brand}</td>
-                <td className="py-4 px-6 text-gray-900">{el.category}</td>
-                <td className="py-4 px-6 text-gray-900">₹{el.price}</td>
-                <td className="py-4 px-6 text-gray-900"> <AiOutlineMinusCircle className=' cursor-pointer' size={20}/></td>
-                <td className="py-4 px-6 text-gray-900">{el.quantity}</td>
-                <td className="py-4 px-6 text-gray-900 ">
-                   <BsPlusCircle size={20} className=' cursor-pointer'/> 
+              {
+                cartData.map((el)=>(
+                  <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={el.id}>
+                <td class="p-4 w-32">
+                  <img
+                    src={el.image}
+                    alt="cartimg"
+                  />
                 </td>
-                
-                <td className="py-4 px-6 text-gray-900"> <AiOutlineDelete className=' cursor-pointer' size={20}/> </td>
+                <td class="py-4 px-6 font-semibold text-gray-900 dark:text-white">
+                  {el.brand}
+                </td>
+                <td class="py-4 px-6">
+                  <div class="flex items-center space-x-3">
+                    <button
+                      class="inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white rounded-full border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                      type="button"
+                    >
+                      <span class="sr-only">Quantity button</span>
+                      <svg
+                        class="w-4 h-4"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </button>
+                    <div>
+                      <span
+                      class="bg-gray-50 text-center w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      >{el.quantity}</span>
+                    </div>
+                    <button
+                      class="inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white rounded-full border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                      type="button"
+                    >
+                      <span class="sr-only">Quantity button</span>
+                      <svg
+                        class="w-4 h-4"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
+                </td>
+                <td class="py-4 px-6 font-semibold text-gray-900 dark:text-white">
+                ₹{el.price}
+                </td>
+                <td class="py-4 px-6">
+                  <a
+                    href="/"
+                    class="font-medium text-red-600 dark:text-red-500 hover:underline"
+                  >
+                    Remove
+                  </a>
+                </td>
               </tr>
-              ))
-
-             }
-            
-            
-              
+                ))
+              }
+           
+             
+             
             </tbody>
           </table>
         </div>
